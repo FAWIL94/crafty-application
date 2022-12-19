fa7e54a18a53a3a03da0e4402320723941a8d1c1
wildfab1 <wildfab1@students.zhaw.ch>
2022-12-19 20:34:30 +0100

    testing MongoDB

---
+++ src/main/java/ch/zhaw/application_crafty/ApplicationCraftyApplication.java
@@ -0,0 +1,13 @@
+package ch.zhaw.application_crafty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationCraftyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationCraftyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/zhaw/application_crafty/ApplicationCraftyApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.zhaw.application_crafty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationCraftyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
